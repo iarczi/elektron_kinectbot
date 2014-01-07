2eb1ce25d357a53b497596d3f2ac0d1978baec0e
Artur Latoszewski <alatosze@starogardzka.(none)>
2014-01-07 13:49:48 +0100

    zmiany w konfiguracji navigation

---
+++ elektron_teleop/devel/setup.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# generated from catkin/cmake/templates/setup.bash.in
+
+CATKIN_SHELL=bash
+. "/home/alatosze/ros_workspace/elektron_kinectbot/elektron_teleop/devel/setup.sh"
---
+++ elektron_teleop/devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/alatosze/ros_workspace/elektron_kinectbot/elektron_teleop/devel/setup.sh"
+emulate zsh # back to zsh mode
